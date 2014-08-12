920f7c42d59befea4d9e848bc9436d81c208f119
Fredrik Andersson <fredrik@kondensator.se>
2014-08-12 18:52:04 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -10,29 +10,47 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 
-var Card = mongoose.model('card', {weight: Number, week: Number, imgUrl: String, time: String})
+var Card = mongoose.model('card', {weight: Number, week: Number, imgUrl: String, time: Date, name: String})
 
 /* GET home page. */
 router.get('/', function(req, res) {
+	
+	Card.findOne({name: 'alex'}, {}, { sort: { 'time' : -1 } }, function (err, NewestAlex) {
   		
-		var q = Card.find().sort([['_id', -1]]).limit(3);
-					q.exec(function(err, posts) {
-						console.log(posts);
-						res.render( 'index', {cards: posts} );
-				});
-		
+		Card.findOne({name: 'alex'}, {}, { sort: { 'time' : 1 } }, function (err, OldestAlex) {
+
+			Card.findOne({name: 'lukas'}, {}, { sort: { 'time' : -1 } }, function (err, NewestLukas) {
+
+				Card.findOne({name: 'lukas'}, {}, { sort: { 'time' : 1 } }, function (err, OldestLukas) {
+
+					res.render('index', {newestAlex: NewestAlex, oldestAlex: OldestAlex, newestLukas: NewestLukas, oldestLukas: OldestLukas});
+
+					});
+
+			});
+
+		});
+
+	});
+
 
 	
 });
 
-router.get('/newCard', function(req, res) {
+router.get('/newCardAlex', function(req, res) {
   
-		res.render( 'newCard');
+		res.render( 'newCard', {name: "alex" });
+	
+});
+
+router.get('/newCardLukas', function(req, res) {
+  		
+		res.render( 'newCard', {name: "lukas"});
 	
 });
 
 router.post('/card-upload', function (req, res) {
-	console.log(req.body.week);
+	
 	var tmp_path = req.files.thumbnail.path;
 	var target_path = './public/images/' + req.files.thumbnail.name;
 	var imgurl = '/images/' + req.files.thumbnail.name;
@@ -42,10 +60,12 @@ router.post('/card-upload', function (req, res) {
 		// delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
 		fs.unlink(tmp_path, function() {
 			if (err) throw err;
+
 			var week = req.body.week;
 			var weight = req.body.weight;
+			var name = req.body.name;
 			var imgUrl = imgurl;
-			var card = new Card({week: week, weight: weight, imgUrl: imgUrl, time:  moment().format('DD/MM-HH:MM')})
+			var card = new Card({week: week, weight: weight, name: name, imgUrl: imgUrl, time:  Date.now()})
 			card.save(function (err) {
 				if (err)
 					console.log(err);
